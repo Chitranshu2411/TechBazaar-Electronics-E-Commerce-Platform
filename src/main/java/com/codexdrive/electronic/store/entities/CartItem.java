3e6a36e07df7bce474da49eb5d77a15350896437
Chitranshu <chitranshu2411@gmail.com>
2026-06-08 17:23:43 +0530

    add CartItem entity for cart product associations
     add CartItem entity for cart product associations

---
+++ src/main/java/com/codexdrive/electronic/store/entities/CartItem.java
@@ -0,0 +1,31 @@
+package com.codexdrive.electronic.store.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "cart_item")
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int cardItemId;
+
+    @OneToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+    private int quantity;
+    private int totalPrice;
+
+    // mapping cart
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "card_id")
+    private Cart cart;
+
+
+}
